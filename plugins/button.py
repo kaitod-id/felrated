ca5c2598930fd8c0b3693be4a0362707b3adb88b
kaitod-id <108166655+kaitod-id@users.noreply.github.com>
2023-07-21 04:38:40 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -5,13 +5,18 @@ from pyrogram.types import InlineKeyboardButton
 def start_button(client, is_fsub=False):
     buttons = []
 
+    if FORCE_SUB_GROUP:
+        buttons.append([
+            InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
+        ])
+
     if FORCE_SUB_CHANNEL2:
         buttons.append([
-            InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink3),
+            InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
         ])
 
     if FORCE_SUB_CHANNEL and not is_fsub:
-        if FORCE_SUB_CHANNEL2:
+        if FORCE_SUB_GROUP:
             buttons[-1].append(
                 InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink)
             )
@@ -19,15 +24,6 @@ def start_button(client, is_fsub=False):
             buttons.append([
                 InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
             ])
-
-    if FORCE_SUB_GROUP:
-        buttons.append([
-            InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
-        ])
-
-    # Rearrange buttons to be side by side (sejajar/berdampingan)
-    if len(buttons) >= 2:
-        buttons = [button_row for button_row in buttons]
 
     buttons.extend([
         [
@@ -42,24 +38,25 @@ def start_button(client, is_fsub=False):
 def fsub_button(client, message):
     buttons = []
 
-    if FORCE_SUB_CHANNEL2:
-        buttons.append([
-            InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink3),
-        ])
-
-    if FORCE_SUB_CHANNEL:
-        buttons.append([
-            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-        ])
-
     if FORCE_SUB_GROUP:
         buttons.append([
             InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink2),
         ])
 
-    # Rearrange buttons to be side by side (sejajar/berdampingan)
-    if len(buttons) >= 2:
-        buttons = [button_row for button_row in buttons]
+    if FORCE_SUB_CHANNEL2:
+        buttons.append([
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
+        ])
+
+    if FORCE_SUB_CHANNEL:
+        if FORCE_SUB_GROUP:
+            buttons[-1].append(
+                InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink)
+            )
+        else:
+            buttons.append([
+                InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
+            ])
 
     try:
         if message.command and len(message.command) > 1:
